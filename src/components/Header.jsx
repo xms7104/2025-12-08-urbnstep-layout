c0c29e40c0981bfee938ac28124fab3f2f414560
xms_7104 <xms881126@gmail.com>
2025-12-08 15:36:05 +0800

    feat: add Header component and styles

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+function Header(){
+    return(
+        <div className='header_block'>
+            <div className='logo_block'>
+                <p>URBNSTEP</p>
+            </div>
+            <div className='user_button_block'>
+                <button className='user_button'>
+                    <FontAwesomeIcon icon={faUser} size='xl' />
+                </button>
+            </div>
+            <div className='button_block'>
+                <button className='menu_button'>商品列表</button>
+                <button className='menu_button'>品牌故事</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
