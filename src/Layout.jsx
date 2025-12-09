d39e20339e24ce0d2454cc048ef33d5955cb34ec
xms_7104 <xms881126@gmail.com>
2025-12-09 10:17:07 +0800

    feat: add SalesList component with associated styles

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,14 +1,20 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { Outlet } from 'react-router-dom';
+import SalesList from './components/SalesList';
+import { Outlet, useLocation } from 'react-router-dom';
 
 function Layout() {
+  const location = useLocation();
+  const hideSalesBlockRoutes = ['/cart', '/login', '/signup'];
+  const shouldHideSalesBlock = hideSalesBlockRoutes.includes(location.pathname);
+
   return (
     <div>
       <Header />
       <main>
         <Outlet />
       </main>
+      {!shouldHideSalesBlock && <SalesList />}
       <Footer />
     </div>
   );
