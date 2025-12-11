873dcb28d25710cafda0367e66de7ca8a8d9038a
xms_7104 <xms881126@gmail.com>
2025-12-11 09:57:19 +0800

    feat:add Product Description component with styles

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,5 +1,6 @@
 import ProductImage from "../components/ProductDetail/ProductImg";
 import ProductCategoryList from "../components/UI/ProductCategoryList";
+import ProductDescription from "../components/ProductDetail/ProductDescription";
 
 function ProductDetail(){
     const categoryList = ['首頁', '女鞋', '滑板鞋', 'Platform 404'];
@@ -14,6 +15,7 @@ function ProductDetail(){
                     <div className="product_category_list_category_text_block">
                         <ProductCategoryList categoryArr={categoryList} />
                     </div>
+                    <ProductDescription />
                 </div>
             </div>
             <div>下</div>
