29e1c5afd1f50c10b57220abc14e713978be4eba
xms_7104 <xms881126@gmail.com>
2025-12-15 01:06:48 +0800

    feat: add product list component and update product page styles

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,6 +1,35 @@
-
+import { useState, useEffect } from "react";
+import ProductCategoryList from "../components/UI/ProductCategoryList"
+import MenuList from "../components/Product/MenuList";
+import ProductList from "../components/Product/ProductList";
+import Pagination from "../components/Product/Pagination";
 
 function Product(){
+    const [categoryList, setCategoryList] = useState(['首頁', '女鞋', '所有商品']);
+    const [currentCategory, setCurrentCategory] = useState('所有商品');
+
+    useEffect(() => {
+        setCategoryList(prev => [...prev.slice(0, prev.length - 1), currentCategory]);
+    }, [currentCategory])
+
+    return(
+        <div className="product_block">
+            <div className="product_category_list_block product_category_list_category_text_block">
+                <ProductCategoryList categoryArr={categoryList} />
+            </div>
+            <div className="title_block">
+                <p className="title_text">女鞋</p>
+            </div>
+            <div className="product_container">
+                <div className="menulist_block">
+                    <MenuList currentCategory={currentCategory} setCurrentCategory={setCurrentCategory} />
+                </div>
+                <div className="product_list_block">
+                    <ProductList />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Product
