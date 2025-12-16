a096dbebe869b8e1961243c03e6118b77b869168
xms_7104 <xms881126@gmail.com>
2025-12-16 09:53:24 +0800

    feat: add router navigation and refine styles

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 function Banner(){
     return(
         <div className="banner_container_block">
@@ -6,7 +8,9 @@ function Banner(){
                     <p className="banner_title">URBNSTEP</p>
                     <p className="banner_subTitle">Your Step, Your Statement</p>
                 </div>
-                <button className="banner_button">立即選購</button>
+                <Link to='/productDetail'>
+                    <button className="banner_button">立即選購</button>
+                </Link>
             </div>
         </div>
     )
--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -1,12 +1,17 @@
 import Logo from '../../assets/logo.svg';
+import { Link } from 'react-router-dom';
 
 function Footer(){
     return(
         <div className='footer_block'>
             <div className='footer_menu_block'>
-                <img src={Logo} alt='logo' />
+                <Link to="/">
+                    <img src={Logo} alt='logo' />
+                </Link>
                 <div className='footer_button_block'>
-                    <button className='menu_button btn-footer'>商品列表</button>
+                    <Link to="/product">
+                        <button className='menu_button btn-footer'>商品列表</button>
+                    </Link>
                     <button className='menu_button btn-footer'>品牌故事</button>
                 </div>
             </div>
